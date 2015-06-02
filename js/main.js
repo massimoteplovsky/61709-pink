e5c1d28f8a09f6e4aebea3a3edb47289094cd685
massimoteplovsky <www.maximalist@mail.ru>
2015-06-02 22:52:17 +0400

    module8-task1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,67 @@
+
+
 (function() {
+
+//numeric fields updating
+
+var elements = document.querySelectorAll(".input-numeric");
+
+for (var i = 0; i < elements.length; i++) {
+
+	counter(elements[i]);
+
+	}
+
+	function counter(parent){
+
+		var input = parent.querySelector("input");
+		var minus = parent.querySelector(".minus");
+		var plus = parent.querySelector(".plus");
+
+		minus.addEventListener("click", function() {
+
+ 			changeValue(false);
+
+ 		});
+
+ 		plus.addEventListener("click", function() {
+
+ 			changeValue(true);
+
+ 		});
+
+ 		function changeValue(operation){
+
+			var fieldValue = Number(input.value);
+
+			if(isNaN(fieldValue)){
+				fieldValue = 0;
+			}
+
+			if (operation) { 
+				minus.classList.remove("disable");
+				input.value = fieldValue + 1; 
+
+			} else { 
+				if(fieldValue <= 1){
+					input.value = 0;
+					minus.classList.add("disable");
+				} else{
+			 		input.value = fieldValue - 1; 
+				}
+
+
+			 }
+
+		}
+
+	}
+
+
+
+
+
+// form sending
 
  if (!("FormData" in window)) { 
 
@@ -22,7 +85,7 @@
 
  if (xhr.readyState == 4) {
 
- alert(xhr.responseText); 
+ 	console.log(xhr.responseText); 
 
  }
 
@@ -31,5 +94,7 @@
  xhr.send(data); 
 
  });
+
+ 
 
 })();
