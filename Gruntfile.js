72c4aaf5ac86e119c61c6c2741427904277bfb9a
massimoteplovsky <www.maximalist@mail.ru>
2015-06-10 18:16:59 +0400

    module10-task1

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,16 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-githooks');
   grunt.loadNpmTasks('grunt-lintspaces');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-csscomb');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+  require("load-grunt-tasks")(grunt);
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -18,12 +25,33 @@ module.exports = function(grunt) {
       }
     },
 
+    autoprefixer: { 
+      options: { 
+        browsers: ["last 2 version", "ie 10"] 
+      }, 
+        style: { 
+          src: "build/css/style.css" 
+        }
+    },
+
     sass: {
       style: {
         files: {
-          'css/style.css': 'sass/style.scss'
+          'build/css/style.css': 'source/sass/style.scss'
         }
       }
+    },
+
+    imagemin: { 
+      images: { 
+        options: { 
+          optimizationLevel: 3 
+        }, 
+          files: [{ 
+            expand: true, 
+            src: ["build/img/**/*.{png,jpg,gif,svg}"] 
+          }] 
+        } 
     },
 
     lintspaces: {
@@ -47,26 +75,62 @@ module.exports = function(grunt) {
     },
 
     copy: {
-      gosha: {
+      build: {
         files: [{
           expand: true,
+          cwd: "source",
           src: [
             '*.html',
             'css/**',
             'img/**',
             'js/**'
           ],
-          dest: 'gosha',
+          dest: 'build',
         }]
       }
     },
 
     clean: {
-      gosha: [
-        'gosha/img/README',
-        'gosha/js/README'
-      ]
+        build: ["build"]
+    },
+
+    csscomb: { 
+      style: { 
+        expand: true, 
+        src: ["sass/**/*.sass"] 
+      }
+    },
+
+    cssmin: { 
+      style: { 
+        options: { 
+          keepSpecialComments: 0, 
+          report: "gzip" 
+        }, 
+        files: { 
+          "build/css/style.min.css": ["build/css/style.css"] 
+        } 
+      }
+    },
+
+    htmlmin: { 
+      options: { 
+        removeComments: true, 
+        collapseWhitespace: true, 
+        collapseBooleanAttributes: true, 
+        caseSensitive: true, 
+        keepClosingSlash: false 
+      }, 
+      html: { 
+      files: { 
+        "build/index.min.html": "build/index.html", 
+        "build/post.min.html": "build/post.html",
+        "build/blog.min.html": "build/blog.html",
+        "build/form.min.html": "build/form.html",
+      } 
+      } 
     }
+
   });
 
   grunt.registerTask('test', ['lintspaces:test']);
@@ -78,4 +142,14 @@ module.exports = function(grunt) {
   } else {
     grunt.registerTask('gosha', ['copy:gosha', 'clean:gosha']);
   }
+
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "sass",
+    "autoprefixer",
+    "cssmin",
+    "htmlmin",
+    "imagemin"
+    ]);
 };
---
+++ source/js/main.js
@@ -0,0 +1,136 @@
+
+
+(function() {
+
+var toggleLink = document.querySelector(".page-header__toggle");
+var menu = document.querySelector(".main-nav__list");; 
+
+toggleLink.addEventListener("click", function(event){
+	event.preventDefault();
+	
+	menu.classList.toggle("visible");
+
+	if(menu.classList.contains("visible")){
+		this.classList.remove("page-header__toggle");
+		this.classList.add("page-header__cross")
+	} else {
+		this.classList.add("page-header__toggle");
+		this.classList.remove("page-header__cross");
+	}
+
+});
+
+//numeric fields updating
+
+var elements = document.querySelectorAll(".input-numeric");
+var area = document.querySelector(".page-form__companion-blocks");
+var companion = document.querySelector(".page-form__companion-block");
+var template = document.querySelector("#companion").innerHTML;
+
+for (var i = 0; i < elements.length; i++) {
+
+	counter(elements[i]);
+
+	}
+
+	function counter(parent){
+
+		var input = parent.querySelector("input");
+		var minus = parent.querySelector(".minus");
+		var plus = parent.querySelector(".plus");
+
+
+		minus.addEventListener("click", function() {
+
+ 			changeValue(false);
+
+ 		});
+
+ 		plus.addEventListener("click", function() {
+
+ 			changeValue(true);
+
+ 		});
+
+ 		function changeValue(operation){
+
+			var fieldValue = Number(input.value);
+
+			if(isNaN(fieldValue)){
+				fieldValue = 0;
+			}
+
+			if (operation) { 
+				minus.classList.remove("disable");
+				input.value = fieldValue + 1; 
+				if(plus.classList.contains("addRemove")){
+					area.innerHTML = area.innerHTML + template;
+				}	
+
+
+			} else { 
+				if(fieldValue <= 1){
+					input.value = 0;
+					minus.classList.add("disable");
+				} else{
+			 		input.value = fieldValue - 1; 
+			 		if(minus.classList.contains("addRemove")){
+			 			area.removeChild(companion);
+			 		}
+				}
+
+
+			 }
+
+		}
+
+	}
+
+
+
+
+
+// form sending
+
+if (!("FormData" in window)) { 
+
+return; 
+
+} 
+
+var form = document.querySelector(".page-form"); 
+
+form.addEventListener("submit", function(event) {
+
+ 	event.preventDefault();
+
+	var data = new FormData(form); 
+
+	request(data, function(response) { 
+
+ 		console.log(response); 
+
+ 	});
+
+}); 
+
+function request(data, fn){
+
+	var xhr = new XMLHttpRequest(); 
+	var time = (new Date().getTime());
+
+	xhr.open("post", "http://simonenko.su/academy/echo") + time; 
+
+ 	xhr.addEventListener("readystatechange", function() {
+
+		if (xhr.readyState == 4) {
+
+ 			fn(xhr.responseText); 
+
+ 		} 
+	});
+
+	xhr.send(data);
+}	
+
+})();
